e478033983308a6ad6d6977a8f820224e57930f3
andyfe@gmail.com <andyfe@gmail.com>
2024-07-18 11:54:50 +0000

    started qa component

---
+++ client_code/comp/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import compTemplate
+from anvil import *
+from .. import api
+
+class comp(compTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+  def ask_click(self, **event_args):
+    status, body = api.post(f"qa?query={self.question.text}")
+    if status == 200:
+      self.response = body['results'][0]
+      self.answer.text = self.response['response']
+      #self.sources_panel.items = self.response['sources']
