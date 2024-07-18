943de0dd85d69bf9afead4eb4b9d3bb36f365368
andyfe@gmail.com <andyfe@gmail.com>
2024-07-18 14:40:34 +0000

    Edited settings and data

--- client_code/data/__init__.py
+++ client_code/data/__init__.py
@@ -15,11 +15,15 @@ class data(dataTemplate):
   def upload_data_change(self, files, **event_args):
     self.upload_data.clear()
     embed = confirm("Run embedding on the documents?")
+    
     with Notification(f"Uploading {len(files)} files"):
       items = self.data_panel.items
+      count = 0
       for file in files:
         status, body = api.upload(f"data/upload?embed={embed}", file.get_bytes(), file.name, file.content_type)
         if status == 200:
           items.insert(0, body['results'][0])
+        count += 1
+        print(count, len(files))
     self.data_panel.items = items
 
